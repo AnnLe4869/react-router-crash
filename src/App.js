b43d4b0fa2005b87441a7fb6c69774f3bb0fcd9c
Anh Le <kunquan2345@gmail.com>
2020-02-15 04:27:55 -0800

    Small stuff with Router, Route and Link
    - Note how we use the Route to render a component
      We can use children component - thing we did in code (this is the recommended way)
      Or we can use render property or children property

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [writers, setWriters] = useState([]);
-  return <div className="App"></div>;
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await (
+          await fetch("http://localhost:3004/writers")
+        ).json();
+        setWriters([...writers, ...data]);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Router>
+      <>
+        <ul>
+          <li key="home">
+            <Link to="/">Home</Link>
+          </li>
+          <li key="writers">
+            <Link to="/writers">Writers</Link>
+          </li>
+        </ul>
+      </>
+      <>
+        <Route exact path="/">
+          <div>Hello World</div>
+        </Route>
+        <Route path="/writers">
+          <ul>
+            {writers.map(writer => (
+              <li key={writer.id}>{writer.name}</li>
+            ))}
+          </ul>
+        </Route>
+      </>
+    </Router>
+  );
 }
 
 export default App;
