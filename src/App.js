f4cc51f3c5d310f7b8a108b9be5dcad8c2bc64df
Anh Le <kunquan2345@gmail.com>
2020-02-15 02:41:18 -0800

    Set up fake server with json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [writers, setWriters] = useState([]);
   return <div className="App"></div>;
 }
 
